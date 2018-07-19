c70f7bde6dc29b32a62407af8b13e820ca67e6dd
digshake <dsshook@gmail.com>
2018-07-18 19:16:10 -0500

    Add files via upload
    Test commit

---
+++ Story.java
@@ -0,0 +1,9 @@
+public class Story {
+    public static void main(String[] args) {
+        int michael = 30;
+        int sally = michael * 2;
+        double bill = sally * (4.0 / 3);
+        double rachel = bill - 52;
+        System.out.println((michael + sally + bill + rachel) / 4);
+    }
+}
